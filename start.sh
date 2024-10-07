d551de7a195cd8b2b830c07478511e9066b8b4a6
cadentj <caden.juang@gmail.com>
2024-10-07 22:41:14 +0000

    fixed ray start script

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
-ray start --head 
+ray start --head \
+    --port=6379 \
+    --include-dashboard=true \
+    --dashboard-host=0.0.0.0 \
+    --dashboard-port=8265 \
 
-serve run app.main:head
+serve deploy ray_config.yml
