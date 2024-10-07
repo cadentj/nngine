7e0f4a5534eb52d0a4e27ccc0db8e3c1b193755b
Caden <35741667+cadentj@users.noreply.github.com>
2024-10-07 18:12:31 -0400

    Check code works

--- app/main.py
+++ app/main.py
@@ -1,14 +1,26 @@
 from fastapi import FastAPI
 from ray import serve
 
+from .model import ModelDeployment
+from .core.schema import NNsightRequestModel
+
 app = FastAPI()
 
 @serve.deployment
 @serve.ingress(app)
 class FastAPIDeployment:
+    def __init__(self, model):
+        self.model = model
+
     # FastAPI will automatically parse the HTTP request for us.
     @app.get("/hello")
     def say_hello(self, name: str) -> str:
         return f"Hello {name}!"
+    
+    @app.post("/code")
+    async def remote(self, request: NNsightRequestModel):
+        return await self.model.remote(request)
 
-head = FastAPIDeployment.bind()
+
+model = ModelDeployment.bind("Qwen/Qwen2.5-0.5B-Instruct")
+head = FastAPIDeployment.bind(model)
